f44e5341bb6ffc03d8dfea7e7c86ed04769da043
AswathSiva2005 <aswathsiva0420@gmail.com>
2025-11-19 19:27:37 +0530

    atlas uri changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/fitness_tracker", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB Connected (Compass)");
+    await mongoose.connect(
+      "mongodb+srv://aswathsiva0420:aswathsiva0420@cluster0.l4kdjc2.mongodb.net/fitness_tracker?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("✅ MongoDB Connected (Atlas)");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error);
     process.exit(1);
